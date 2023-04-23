8dc3db8b657bb4bae8ec2d1ef9576e24b26f6bc8
Mathis Fortin <mathisfortin@outlook.com>
2023-04-22 21:50:33 -0400

    Modifications 2023-04-22 21:50:33

--- impex/impex.py
+++ impex/impex.py
@@ -1,31 +1,19 @@
-"""
-TP2
-Noms : Mathis Fortin et Félix Chamberland
-Groupe : 00002
-Travail réalisé dans le cadre du cours "420 SD2-HY Programmation orientée objet" donné par M. Pier Luc Ducharme
-Dernière modification : 2023-04-22
-Version 1
-"""
-#FIXME: Le prof a dit d'importer client.client.py mais comment?
-import client.py
-
 #Lecture du fichier CSV contenant les utilisateurs, leur type d'utilisateur et leur mot de passe
-with open('data/utilisateurs.csv', 'rt') as fichier_utilisateur:
+with open('data/Utilisateur.CSV', 'rt') as fichier_utilisateur:
     fichier_utilisateur.read()
 
 #Lecture du fichier CSV contenant les chalets
-with open('data/chalets.csv', 'rt') as fichier_chalets:
+with open('data/Chalet.CSV', 'rt') as fichier_chalets:
     fichier_chalets.read()
 
 #Lecture du fichier XML contenant les disponibilités des chalets
-with open('data/disponibilites.xml', 'rt') as fichier_dispo_chalets:
+with open('data/Disponibilités chalet.XML', 'rt') as fichier_dispo_chalets:
     fichier_dispo_chalets.read()
 
 #Lecture du fichier XML contenant les réservations
-with open('data/reservations.xml', 'rt') as fichier_reservations:
+with open('data/Réservations.XML', 'rt') as fichier_reservations:
     fichier_reservations.read()
 
-#TODO: Méthodes pour exporter les données
 #Méthode pour exporter les données de réservations dans un fichier "export_{timestamp}.csv" dans le format CSV
 
 #Méthode pour exporter les données de réservations dans un fichier "export_{timestamp}.json" dans le format JSON
--- serveur/super_chalets_serveur.py
+++ serveur/super_chalets_serveur.py
@@ -1,235 +1,41 @@
-"""
-TP2
-Noms : Mathis Fortin et Félix Chamberland
-Groupe : 00002
-Travail réalisé dans le cadre du cours "420 SD2-HY Programmation orientée objet" donné par M. Pier Luc Ducharme
-Dernière modification : 2023-04-28 à 22h00
-Version 1
-"""
-#TODO: Tests unaires pour chaque méthode
-#TODO: Gestion de cache
-#TODO: @Mathis: Ajouter des commentaires
-
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 
-class SuperChalet():
-    Reservations.liste_reservation()
-    Chalets.liste_chalets()
-    Utilisateurs.liste_utilisateurs()
+class Reservation:
 
-
-class Reservations:
-
-    def __init__(self, id, chalet, utilisateur):
-        if self.__reservations == None:
-            self.__reservation = []
-        self.__id = id
-        self.__chalet = chalet
-        self.__utilisateur = utilisateur
-        self.__reservations = self.__reservations.append([self.__id, self.__chalet, self.__utilisateur])
+    def __init__(self):
+        self.__reservation = {}
 
     @property
-    def id(self):
-        return self.__id
+    def reservation(self):
+        return self.__reservation
 
-    @property
-    def chalet(self):
-        return self.__chalet
+    def ajout_chalet(self, nom):
+        if nom in self.__chalet.keys():
+            raise ValueError('Chalet existant')
+        self.__chalet[nom] = []
 
-    @property
-    def utilisateur(self):
-        return self.__utilisateur
+    def ajout_animal(self, enclos, nom):
+        if enclos not in self.__enclos.keys():
+            raise ValueError('Enclos inexistant')
+        self.__enclos[enclos].append(nom)
 
-    def obtenir_infosreservation(self, id):
-        for x in range(len(self.__reservations)):
-            infosreservation = self.__reservations[x]
-            if y.index(id) != -1:
-                print(infosreservation)
-#TODO: Ajouter cas si plusieurs et si aucune réservation
-
-    def obtenir_reservations(self, utilisateur):
-        for x in range(len(self.__reservations)):
-            infosreservation = self.__reservations[x]
-            if y.index(utilisateur) != -1:
-                print(infosreservation)
-
-    def ajout_reservation(self, id, chalet, utilisateur):
-        if id not in self.__id :
-            raise ValueError('Cette réservation existe déjà')
-        else:
-            self.__reservations = self.__reservations.append([id, chalet, utilisateur])
-#FIXME: Je ne sais pas comment ajouter l'objet Chalet et l'objet Utilisateur dans le liste de la classe objet Réservation
-#FIXME: Je ne sais pas si nous devons vérifier les disponibilités pour ajouter une réservation
-
-    def remplacer_reservation(self, id_reservation_a_remplacer):
-        pass
-#TODO: @Mathis: Méthode pour remplacer une réservation
-
-
-    def supprimer_reservation(self, id):
-        for x in range(len(self.__reservations)):
-            infosreservation = self.__reservations[x]
-            if infosreservation.index(id) != -1:
-                del(self.__reservations[x])
-
-    def liste_reservation(self):
-        return(self.__reservations)
-
-class Chalets:
-
-    def __init__(self, id, nom, url_image, geolocalisation):
-        if self.__chalets == None:
-            self.__chalets = []
-        self.__id = id
-        self.__nom = nom
-        self.__url_image = url_image
-        self.__geolocalisation = geolocalisation
-        self.__chalets = self.__chalets.append([self.__id, self.__nom, self.__url_image, self.__geolocalisation])
-
-    @property
-    def id(self):
-        return self.__id
-
-    @property
-    def nom(self):
-        return self.__nom
-
-    @property
-    def url_image(self):
-        return self.__url_image
-
-    @property
-    def geolocalisation(self):
-        return self.__geolocalisation
-
-    def ajout_chalet(self, id, nom, url_image, geolocalisation):
-            if id in self.__id and if nom in self.__nom and if geolocalisation in self.__geolocalisation :
-                raise ValueError('Ce chalet existe déjà')
-            else:
-                self.__chalets = self.__chalets.append([id, nom, url_image, geolocalisation])
-#FIXME: Je ne sais pas comment ajouter l'objet Géolocalisation dans la liste de la classe Chalet
-
-class Geolocalisation_Chalet:
-
-    def __init__(self, latitude, longitude):
-        self.__latitude = latitude
-        self.__longitude = longitude
-
-    @property
-    def latitude(self):
-        return self.__latitude
-
-    @property
-    def longitude(self):
-        return self.__longitude
-
-class Utilisateurs:
-
-
-    def __init__(self, email, mot_de_passe, nom, prenom, adresse):
-        if self.__utilisateurs == None:
-            self.__utilisateurs = []
-        self.__email = email
-        self.__mot_de_passe = mot_de_passe
-        self.__nom = nom
-        self.__prenom = prenom
-        self.__adresse = adresse
-        self.__utilisateurs = self.__utilisateurs.append([email, mot_de_passe, nom, prenom, [no_civique, rue, ville, province, pays, code_postal]])
-        adresse = Adresses(no_civique, rue, ville, province, pays, code_postal)
-#FIXME : Je ne sais pas comment ajouter l'objet Adresse dans le liste de la classe objet Adresse
-
-    @property
-    def email_utilisateur(self):
-        return self.__email
-
-    @property
-    def mot_de_passe_utilisateur(self):
-        return self.__mot_de_passe
-
-    @property
-    def nom_utilisateur(self):
-        return self.__nom
-
-    @property
-    def prenom_utilisateur(self):
-        return self.__prenom
-
-    @property
-    def adresse_utilisateur(self):
-        return self.__adresse
-
-    def ajout_utilisateur(self, email, mot_de_passe, nom, prenom, no_civique, rue, ville, province, pays, code_postal):
-        if email in self.__email and if nom in self.__nom and if prenom in self.__prenom:
-            raise ValueError('Cet utilisateur existe déjà')
-        else:
-            self.__utilisateurs = self.__utilisateurs.append([email, mot_de_passe, nom, prenom, [no_civique, rue, ville, province, pays, code_postal]])
-            adresse = Adresses(no_civique, rue, ville, province, pays, code_postal)
-#FIXME: Je ne sais pas comment ajouter l'objet Adresse dans le liste de la classe objet Adresse
-
-    def obtenir_infosutilisateur(self, email):
-        if email not in self.__utilisateurs:
-            raise ValueError("Aucun utilisateur n'a le courriel suivant:" + email)
-        else:
-            for x in range(len(self.__utilisateurs)):
-                infosutilisateur = self.__reservations[x]
-                if infosutilisateur.index(email) != -1:
-                    print(infosreservation)
-
-
-class Adresses:
-
-    def __init__(self, no_civique, rue, ville, province, pays, code_postal):
-        self.__no_civique = no_civique
-        self.__rue = rue
-        self.__ville = ville
-        self.__province = province
-        self.__pays = pays
-        self.__code_postal = code_postal
-        self.__adresse = [self.__no_civique, self.__rue, self.__ville, self.__province, self.__pays, self.__code_postal]
-
-    @property
-    def no_civique(self):
-        return self.__no_civique
-
-    @property
-    def rue(self):
-        return self.__rue
-
-    @property
-    def ville(self):
-        return self.__ville
-
-    @property
-    def province(self):
-        return self.__province
-
-    @property
-    def pays(self):
-        return self.__pays
-
-    @property
-    def code_postal(self):
-        return self.__code_postal
-
-#TODO: Toute cette partie est à regarder, je ne la comprends pas.
 class TPBaseHTTPRequestHandler(BaseHTTPRequestHandler):
 
     # variable de classe
-SuperChalet = SuperChalet()
-#MATHIS: Il faudrait faire des if path.startswith('/enclos/') avec les différents trucs. Même chose pour le do_POST. Il faudrait faire un do_DELETE et un do_PUT aussi
-    def do_GET(self):
+    chalet = Chalet()
+    def do_GET_reservationID(self):
         headers = self.headers
         path = self.path
-        if path.startswith('/enclos/'):
+        if path.startswith('/chalet/'):
             enclos = path.split('/')[2]
-            content = 'Enclos: ' + enclos + ' -> ' + str(self.zoo.enclos[enclos])
+            content = 'Chalet: ' + chalet + ' -> ' + str(self.zoo.chalet[chalet])
             self.send_response(200)
             self.end_headers()
             self.wfile.write(bytes(content, 'utf-8'))
         else:
-            self.send_response(500, 'enclos non trouve')
+            self.send_response(500, 'Chalet non trouve')
             self.end_headers()
 
     def do_POST(self):
@@ -273,4 +79,28 @@ from client import client
 from http.server import HTTPServer
 # Le serveur va ecouter sur localhost:8080
 httpd = HTTPServer(('localhost', 8080), client)
+
+#Définition de la classe Chalet
+class Chalet():
+
+    #Constructeur de la classe Chalet
+    def __init__(self):
+        pass
+
+    #Méthode pour ajouter un chalet
+    def do_POST_chalet(self):
+        pass
+
+    #Méthode pour retourner les informations d'un chalet
+    def do_GET_chalet(self):
+        pass
+
+# Commence a 'servir' les connections
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+
+#Code pour les tests
 """
