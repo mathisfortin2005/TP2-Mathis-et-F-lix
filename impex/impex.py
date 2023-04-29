50d34c495546e7b28fff203f071ded1d213fb3ac
Mathis Fortin <mathisfortin@outlook.com>
2023-04-28 22:19:32 -0400

    Modifications 2023-04-28 22:19:27

--- impex/impex.py
+++ impex/impex.py
@@ -6,6 +6,8 @@ Travail réalisé dans le cadre du cours "420 SD2-HY Programmation orientée obj
 Dernière modification : 2023-04-22
 Version 1
 """
+#FIXME: Le prof a dit d'importer client.client.py mais comment?
+import client.py
 
 #Lecture du fichier CSV contenant les utilisateurs, leur type d'utilisateur et leur mot de passe
 with open('data/utilisateurs.csv', 'rt') as fichier_utilisateur:
@@ -16,15 +18,14 @@ with open('data/chalets.csv', 'rt') as fichier_chalets:
     fichier_chalets.read()
 
 #Lecture du fichier XML contenant les disponibilités des chalets
-with open('data/Disponibilités chalet.XML', 'rt') as fichier_dispo_chalets:
+with open('data/disponibilites.xml', 'rt') as fichier_dispo_chalets:
     fichier_dispo_chalets.read()
 
 #Lecture du fichier XML contenant les réservations
-with open('data/Réservations.XML', 'rt') as fichier_reservations:
+with open('data/reservations.xml', 'rt') as fichier_reservations:
     fichier_reservations.read()
 
-
-#MATHIS : Je ne comprends pas les 2 derniers trucs à faire
+#TODO: Méthodes pour exporter les données
 #Méthode pour exporter les données de réservations dans un fichier "export_{timestamp}.csv" dans le format CSV
 
 #Méthode pour exporter les données de réservations dans un fichier "export_{timestamp}.json" dans le format JSON
