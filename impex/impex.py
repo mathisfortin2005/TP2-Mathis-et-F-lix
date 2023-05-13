3d3fedcc5e44c2f23ca8aa5a542718210c820383
felixchamberland <filchamb@gmail.com>
2023-05-12 20:28:29 -0400

    Mon commit

--- impex/impex.py
+++ impex/impex.py
@@ -8,7 +8,7 @@ Version 1
 """
 # FIXME: Pourquoi importer .client.client.py
 
-from client import client # Importation module client (point 1 consignes impex)
+from client import client  # Importation module client (point 1 consignes impex)
 from datetime import datetime
 import csv
 import json
@@ -34,7 +34,7 @@ with open('data/reservations.xml', 'rt') as fichier_reservations:
 class Impex:
     @staticmethod
     # Méthode statique pour exporter les données d'un objet JSON "utilisateur" dans un fichier "export_{timestamp}.csv" dans le format CSV
-    def export_csv(utilisateurs_json):
+    def exportCsv(utilisateurs_json):
         # Pour créer un nom de fichier en fonction du temps
         timestamp = datetime.time().strftime("%Y-%m-%d_%H-%M-%S")
         nom_fichier = f'export_{timestamp}.csv'
@@ -53,7 +53,7 @@ class Impex:
 
     @staticmethod
     # Méthode statique pour exporter les données d'un objet JSON "reservation" dans un fichier "export_{timestamp}.xml" dans le format XML
-    def export_json(reservations_csv):
+    def exportJson(reservations_csv):
         # Pour créer un nom de fichier en fonction du temps
         timestamp = datetime.time().strftime("%Y-%m-%d_%H-%M-%S")
         nom_fichier = f'export_{timestamp}.json'
@@ -62,11 +62,17 @@ class Impex:
             json.dump(reservations_csv, fichier_json)
 
 
-def executer():
-    pass
+def executerJson(fichier_reservations):
+    Impex.export_json(fichier_reservations)
+
+
+def executerCsv(fichier_reservations):
+    Impex.export_csv(fichier_reservations)
+
+
 # TODO : @Felix - Fonction (pas une méthode)  `def executer()` qui contient le code à appeler pour l'import des données vers le serveur
 
 
 # Pour que le code se réalise
-Impex.export_json(fichier_utilisateurs)
-Impex.export_json(fichier_reservations)
+executerJson(fichier_reservations)
+executerCsv(fichier_reservations)
